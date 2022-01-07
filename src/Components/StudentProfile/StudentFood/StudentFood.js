f45c9b61643da3573b2326fb774ed484ce605e27
maharabhossain1 <maharabhossain0022@gmail.com>
2022-01-07 22:18:54 +0600

    button added

--- src/Components/StudentProfile/StudentFood/StudentFood.js
+++ src/Components/StudentProfile/StudentFood/StudentFood.js
@@ -1,6 +1,5 @@
 import {
   Box,
-  Button,
   Checkbox,
   Paper,
   Table,
--- src/Components/StudentProfile/StudentTransaction/StudentTransaction.js
+++ src/Components/StudentProfile/StudentTransaction/StudentTransaction.js
@@ -6,6 +6,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Box, Button } from "@mui/material";
+import { HiOutlineArrowLeft } from "react-icons/hi";
 
 export default function StudentTransaction() {
   const rows = [
@@ -35,36 +37,99 @@ export default function StudentTransaction() {
     },
   ];
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead sx={{ border: "none" }}>
-          <TableRow>
-            <TableCell sx={{ fontSize: "1.5rem" }} align="center">
-              নাম
-            </TableCell>
-            <TableCell align="center">লেনদেনের ধরণ</TableCell>
-            <TableCell align="center">টাকার পরিমান</TableCell>
-            <TableCell align="center">ডিস্কাউন্ট</TableCell>
-            <TableCell align="center">ডিস্কাউন্ট</TableCell>
-            <TableCell align="center">মন্তব্য</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => {
-            const { _id, studentName, type, amount } = row;
-            return (
-              <TableRow
-                key={_id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell align="center">{studentName}</TableCell>
-                <TableCell align="center">{type}</TableCell>
-                <TableCell align="center">{amount}</TableCell>
+    <Box>
+      <Box sx={{ width: "90%", m: "auto", my: 5 }}>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead sx={{ border: "none" }}>
+              <TableRow>
+                <TableCell
+                  sx={{
+                    fontSize: "1rem",
+                    fontWeight: "bold",
+                    color: "#616365",
+                  }}
+                  align="center"
+                >
+                  নাম
+                </TableCell>
+                <TableCell
+                  sx={{
+                    fontSize: "1rem",
+                    fontWeight: "bold",
+                    color: "#616365",
+                  }}
+                  align="center"
+                >
+                  লেনদেনের ধরণ
+                </TableCell>
+                <TableCell
+                  sx={{
+                    fontSize: "1rem",
+                    fontWeight: "bold",
+                    color: "#616365",
+                  }}
+                  align="center"
+                >
+                  টাকার পরিমান
+                </TableCell>
+                <TableCell
+                  sx={{
+                    fontSize: "1rem",
+                    fontWeight: "bold",
+                    color: "#616365",
+                  }}
+                  align="center"
+                >
+                  ডিস্কাউন্ট
+                </TableCell>
+                <TableCell
+                  sx={{
+                    fontSize: "1rem",
+                    fontWeight: "bold",
+                    color: "#616365",
+                  }}
+                  align="center"
+                >
+                  ডিস্কাউন্ট
+                </TableCell>
+                <TableCell
+                  sx={{
+                    fontSize: "1rem",
+                    fontWeight: "bold",
+                    color: "#616365",
+                  }}
+                  align="center"
+                >
+                  মন্তব্য
+                </TableCell>
               </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => {
+                const { _id, studentName, type, amount } = row;
+                return (
+                  <TableRow
+                    key={_id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell align="center">{studentName}</TableCell>
+                    <TableCell align="center">{type}</TableCell>
+                    <TableCell align="center">{amount}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+      <Box>
+        <Button className="custom-btn-backward" variant="contained">
+          <HiOutlineArrowLeft
+            style={{ verticalAlign: "middle", fontSize: "1.5rem" }}
+          />
+        </Button>
+      </Box>
+    </Box>
   );
 }
