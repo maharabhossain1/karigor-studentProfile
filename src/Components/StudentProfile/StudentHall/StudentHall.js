35a98d9aee96435272f32052d6a38fa745167b45
maharabhossain1 <maharabhossain0022@gmail.com>
2022-01-07 22:54:13 +0600

    button added in hall section

--- src/Components/StudentProfile/StudentHall/StudentHall.js
+++ src/Components/StudentProfile/StudentHall/StudentHall.js
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 
 import React from "react";
 
@@ -6,7 +6,7 @@ export default function StudentHall() {
   return (
     <Box>
       {/* this box is for hall information and take vacation permission */}
-      <Box>
+      <Box sx={{ my: 2 }}>
         <Grid container spacing={6}>
           <Grid item md={6}>
             <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
@@ -14,8 +14,9 @@ export default function StudentHall() {
                 sx={{
                   color: "#4D53D9",
                   backgroundColor: "#EEEFFF",
-                  px: 6,
                   py: 1.5,
+                  width: "200px",
+                  textAlign: "center",
                   border: "2px solid #4D53D9",
                   borderRadius: "10px",
                 }}
@@ -29,8 +30,9 @@ export default function StudentHall() {
                 sx={{
                   color: "#007BFF",
                   backgroundColor: "#D6EAFF",
-                  px: 6,
                   py: 1.5,
+                  width: "200px",
+                  textAlign: "center",
                   border: "2px solid #007BFF",
                   borderRadius: "10px",
                 }}
@@ -41,7 +43,36 @@ export default function StudentHall() {
               </Box>
             </Box>
           </Grid>
-          <Grid item md={6}></Grid>
+          <Grid item md={6} sx={{ textAlign: "center" }}>
+            <Button
+              style={{
+                color: "#FFFDFB",
+                backgroundColor: "#0FAF51",
+                padding: "13px 0px",
+                width: "200px",
+                border: "none",
+                borderRadius: "10px",
+                fontWeight: "bold",
+                margin: "0px 10px",
+              }}
+            >
+              ছুটির অনুমতি দিন
+            </Button>
+            <Button
+              style={{
+                color: "#FFFDFB",
+                backgroundColor: "#728FB4",
+                padding: "13px 0px",
+                width: "200px",
+                border: "none",
+                borderRadius: "10px",
+                fontWeight: "bold",
+                margin: "0px 10px",
+              }}
+            >
+              হল পরিবর্তন
+            </Button>
+          </Grid>
         </Grid>
       </Box>
     </Box>
